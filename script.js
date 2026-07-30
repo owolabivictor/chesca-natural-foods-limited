862bbce2870e32344d0db826d8099cbf59b4b90a
Owolabi Victor <vowolabi16@gmail.com>
2026-07-30 10:53:26 +0100

    Prevented user scroll when the nav-list is open

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const navList = document.querySelector(".nav-list")
 
 hamburger.addEventListener("click", () => {
     navList.style.left = "0"
+    document.body.classList.add("no-scroll");
 })
 
 navList.addEventListener("click", (e) => {
     if(e.target.classList.contains("fa-arrow-right-from-bracket") || e.target.classList.contains("nav-link")){
         navList.style.left = "-100%"
+        document.body.classList.remove("no-scroll");
     }
 })
